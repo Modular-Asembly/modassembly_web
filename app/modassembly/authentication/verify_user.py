1a16a990ac1389688677747f4c838d8fe91997b5
Luis Galeana <lsgaleana@gmail.com>
2025-01-07 20:00:42 -0600

    Update username to email for login

--- app/modassembly/authentication/verify_user.py
+++ app/modassembly/authentication/verify_user.py
@@ -7,10 +7,11 @@ from app.models.User import User
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def verify_user(username: str, password: str, session: Session) -> User:
-    user = session.query(User).filter(User.username == username).first()
+def verify_user(email: str, password: str, session: Session) -> User:
+    user = session.query(User).filter(User.email == email).first()
     if not user:
-        raise ValueError(f"User not found for username :: {username}")
+        raise ValueError(f"User not found for email :: {email}")
     if not pwd_context.verify(password, str(user.hashed_password)):
-        raise ValueError(f"Invalid password for username :: {username}")
+        raise ValueError(f"Invalid password for email :: {email}")
     return user
+
